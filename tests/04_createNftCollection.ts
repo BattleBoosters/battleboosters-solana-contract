1c66cdb610dc951b7a57f6506542e81204393b60
0xantman <antho74830@gmail.com>
2024-03-08 14:33:07 +0100

    Refactor test suite for modularity and maintainability

---
+++ tests/04_createNftCollection.ts
@@ -0,0 +1,291 @@
+import * as anchor from "@coral-xyz/anchor";
+import {BN, Program, web3} from "@coral-xyz/anchor";
+import { Battleboosters } from "../target/types/battleboosters";
+import {assert} from "chai";
+import airdropSol from "./utils/airdrop_sol";
+import {TOKEN_PROGRAM_ID, AccountLayout, MintLayout, ASSOCIATED_TOKEN_PROGRAM_ID} from '@solana/spl-token';
+const { SystemProgram, SYSVAR_RENT_PUBKEY } = anchor.web3;
+import {mplTokenMetadata, getMplTokenMetadataProgramId} from "@metaplex-foundation/mpl-token-metadata";
+import {MPL_TOKEN_METADATA_PROGRAM_ID} from "@metaplex-foundation/mpl-token-metadata";
+import {Connection, LAMPORTS_PER_SOL, PublicKey, Transaction} from "@solana/web3.js";
+import {before} from "mocha";
+import airdrop_sol from "./utils/airdrop_sol";
+import { sleep } from "@switchboard-xyz/common";
+import {AggregatorAccount, SwitchboardProgram} from "@switchboard-xyz/solana.js";
+import InitializePlayerAccount from "./utils/initialize_player_account";
+import { RandomnessService } from "@switchboard-xyz/solana-randomness-service";
+import * as buffer from "buffer";
+import account_init from "./utils/account_init";
+
+describe.only("Create NFT Collection", () => {
+    const provider = anchor.AnchorProvider.env();
+
+    anchor.setProvider(provider);
+
+    const program = anchor.workspace.Battleboosters as Program<Battleboosters>;
+    let switchboardProgram;
+    const {
+        admin_account,
+        metadata_pubkey,
+        bank_pda,
+        bank_bump,
+        program_pda,
+        program_bump,
+        rarity_pda,
+        rarity_bump,
+        mint_authority_account,
+        authority_bump
+    } = account_init(program);
+
+    it("Admin successfully create Energy NFT Collection" ,async () => {
+
+        const [minter]  = anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                Buffer.from("BattleBoosters"),
+                Buffer.from("mint"),
+                Buffer.from([0])
+            ], program.programId);
+
+
+
+        const [metadata]  = anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                Buffer.from("metadata"),
+                metadata_pubkey.toBuffer(),
+                minter.toBuffer()
+            ], metadata_pubkey);
+
+        const [master_edition]  = anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                Buffer.from("metadata"),
+                metadata_pubkey.toBuffer(),
+                minter.toBuffer(),
+                Buffer.from("edition"),
+            ], metadata_pubkey);
+
+        let token_account = anchor.utils.token.associatedAddress( {mint: minter, owner: mint_authority_account})
+        const [token_record]  = anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                Buffer.from("metadata"),
+                metadata_pubkey.toBuffer(),
+                minter.toBuffer(),
+                Buffer.from("token_record"),
+                token_account.toBuffer()
+            ], metadata_pubkey);
+
+        try {
+
+
+            // const update_cu_ix = anchor.web3.ComputeBudgetProgram.setComputeUnitLimit({units: 1000000});
+            //
+            // const tx_ = new anchor.web3.Transaction().add(update_cu_ix)
+            //    anchor.utils.
+
+
+            const tx = await program.methods.createNftCollection(
+                { energy:{} },
+                "Energy Booster",
+                "EB",
+                "https://battleboosters.com/metadata",
+                500 // 5% royalty
+            )
+                .accounts({
+                    creator: admin_account.publicKey,
+                    program: program_pda,
+                    mintAuthority: mint_authority_account,
+                    minter: minter,
+                    metadata: metadata,
+                    masterEdition: master_edition,
+                    tokenAccount: token_account,
+                    tokenRecord: token_record,
+                    associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+                    sysvarInstructions: anchor.web3.SYSVAR_INSTRUCTIONS_PUBKEY,
+                    rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+                    systemProgram: anchor.web3.SystemProgram.programId,
+                    tokenProgram: TOKEN_PROGRAM_ID,
+                    metadataProgram: MPL_TOKEN_METADATA_PROGRAM_ID
+                }).preInstructions([
+                    anchor.web3.ComputeBudgetProgram.setComputeUnitLimit({ units: 300000 })
+                ])
+                .signers([admin_account])// Include new_account as a signer
+                .rpc();
+        }catch (e) {
+            console.log(e)
+        }
+    })
+
+    it("Mint an nft" ,async () => {
+
+        const player = anchor.web3.Keypair.generate();
+
+        if (provider.connection.rpcEndpoint.includes("localhost") ||
+            provider.connection.rpcEndpoint.includes("http://127.0.0.1:8899") ||
+            provider.connection.rpcEndpoint.includes("http://0.0.0.0:8899")){
+            await airdrop_sol(provider, player.publicKey, 10);
+        }
+
+        let programPDA = await program.account.programData.fetch(program_pda);
+
+        const [minter]  = anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                Buffer.from("BattleBoosters"),
+                Buffer.from("mint"),
+                new BN(programPDA.mintableGameAssetNonce).toBuffer("le", 8)
+            ], program.programId);
+        const [master_edition]  = anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                Buffer.from("metadata"),
+                metadata_pubkey.toBuffer(),
+                minter.toBuffer(),
+                Buffer.from("edition"),
+            ], metadata_pubkey);
+
+        const [metadata]  = anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                Buffer.from("metadata"),
+                metadata_pubkey.toBuffer(),
+                minter.toBuffer()
+            ], metadata_pubkey);
+
+        const [energy_minter]  = anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                Buffer.from("BattleBoosters"),
+                Buffer.from("mint"),
+                Buffer.from([0])
+            ], program.programId);
+
+        const [energy_metadata]  = anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                Buffer.from("metadata"),
+                metadata_pubkey.toBuffer(),
+                energy_minter.toBuffer()
+            ], metadata_pubkey);
+
+        const [energy_master_edition]  = anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                Buffer.from("metadata"),
+                metadata_pubkey.toBuffer(),
+                energy_minter.toBuffer(),
+                Buffer.from("edition"),
+            ], metadata_pubkey);
+
+
+
+        const [shield_minter]  = anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                Buffer.from("BattleBoosters"),
+                Buffer.from("mint"),
+                Buffer.from([1])
+            ], program.programId);
+
+
+
+        const [shield_metadata]  = anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                Buffer.from("metadata"),
+                metadata_pubkey.toBuffer(),
+                shield_minter.toBuffer()
+            ], metadata_pubkey);
+
+        const [shield_master_edition]  = anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                Buffer.from("metadata"),
+                metadata_pubkey.toBuffer(),
+                shield_minter.toBuffer(),
+                Buffer.from("edition"),
+            ], metadata_pubkey);
+        const [points_minter]  = anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                Buffer.from("BattleBoosters"),
+                Buffer.from("mint"),
+                Buffer.from([2])
+            ], program.programId);
+
+        const [points_metadata]  = anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                Buffer.from("metadata"),
+                metadata_pubkey.toBuffer(),
+                points_minter.toBuffer()
+            ], metadata_pubkey);
+
+        const [points_master_edition]  = anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                Buffer.from("metadata"),
+                metadata_pubkey.toBuffer(),
+                points_minter.toBuffer(),
+                Buffer.from("edition"),
+            ], metadata_pubkey);
+
+        const [fighter_minter]  = anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                Buffer.from("BattleBoosters"),
+                Buffer.from("mint"),
+                Buffer.from([3])
+            ], program.programId);
+
+        const [fighter_metadata]  = anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                Buffer.from("metadata"),
+                metadata_pubkey.toBuffer(),
+                fighter_minter.toBuffer()
+            ], metadata_pubkey);
+
+        const [fighter_master_edition]  = anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                Buffer.from("metadata"),
+                metadata_pubkey.toBuffer(),
+                fighter_minter.toBuffer(),
+                Buffer.from("edition"),
+            ], metadata_pubkey);
+
+
+
+
+        let token_account = anchor.utils.token.associatedAddress( {mint: minter, owner: admin_account.publicKey})
+
+        const [token_record]  = anchor.web3.PublicKey.findProgramAddressSync(
+            [
+                Buffer.from("metadata"),
+                metadata_pubkey.toBuffer(),
+                minter.toBuffer(),
+                Buffer.from("token_record"),
+                token_account.toBuffer()
+            ], metadata_pubkey);
+
+
+        try {
+            const tx = await program.methods.mintCollectorPack(
+
+            )
+                .accounts({
+                    creator: admin_account.publicKey,
+                    program: program_pda,
+                    mintAuthority: mint_authority_account,
+                    energyMinter: energy_minter,
+                    energyMetadata: energy_metadata,
+                    energyMasterEdition: energy_master_edition,
+                    minter: minter,
+                    tokenAccount: token_account,
+                    tokenRecord: token_record,
+                    masterEdition: master_edition,
+                    metadata: metadata,
+                    sysvarInstructions: anchor.web3.SYSVAR_INSTRUCTIONS_PUBKEY,
+                    systemProgram: anchor.web3.SystemProgram.programId,
+                    tokenProgram: TOKEN_PROGRAM_ID,
+                    metadataProgram: MPL_TOKEN_METADATA_PROGRAM_ID
+                }).preInstructions([
+                    anchor.web3.ComputeBudgetProgram.setComputeUnitLimit({ units: 1000000 })
+                ])
+                .signers([admin_account]) // Include new_account as a signer
+                .rpc();
+
+            const program_pda_data = await program.account.programData.fetch(program_pda);
+            assert.equal(program_pda_data.mintableGameAssetNonce.eq(new BN(1)), true);
+
+
+        }catch (e) {
+            console.log(e)
+        }
+
+    })
+})
---
+++ tests/utils/account_init.ts
@@ -0,0 +1,54 @@
+import * as anchor from "@coral-xyz/anchor";
+import {MPL_TOKEN_METADATA_PROGRAM_ID} from "@metaplex-foundation/mpl-token-metadata";
+
+const initAccounts = function(program) {
+    const metadata_pubkey = new anchor.web3.PublicKey(MPL_TOKEN_METADATA_PROGRAM_ID);
+
+    const admin_account = anchor.web3.Keypair.fromSecretKey( new Uint8Array([
+            223,  59, 101, 153, 143,  21,  27,  11, 169, 175,  70,
+            197,  18, 124,  44,  79, 218,  51, 196, 199, 144, 211,
+            97,  87,  75, 138,  62, 180, 106, 250, 127, 172,   6,
+            144, 226, 141, 181, 189,  96,  98, 164, 204, 232, 161,
+            130, 182,  19, 162,  30, 200, 230, 194,  32,  45,  49,
+            175, 101, 113,  85, 206, 140,   5, 206, 107
+        ]),
+    )
+    const [bank_pda, bank_bump]  = anchor.web3.PublicKey.findProgramAddressSync(
+        [
+            Buffer.from("BattleBoosters"),
+            Buffer.from("bank"),
+        ], program.programId);
+
+    const [program_pda, program_bump]  = anchor.web3.PublicKey.findProgramAddressSync(
+        [
+            Buffer.from("BattleBoosters"),
+            Buffer.from("program"),
+        ], program.programId);
+
+    const [rarity_pda, rarity_bump]  = anchor.web3.PublicKey.findProgramAddressSync(
+        [
+            Buffer.from("BattleBoosters"),
+            Buffer.from("rarity"),
+        ], program.programId);
+
+    const [mint_authority_account, authority_bump]  = anchor.web3.PublicKey.findProgramAddressSync(
+        [
+            Buffer.from("BattleBoosters"),
+            Buffer.from("mintAuthority"),
+        ], program.programId);
+
+    return {
+        admin_account,
+        metadata_pubkey,
+        bank_pda,
+        bank_bump,
+        program_pda,
+        program_bump,
+        rarity_pda,
+        rarity_bump,
+        mint_authority_account,
+        authority_bump
+    }
+}
+
+export default initAccounts
