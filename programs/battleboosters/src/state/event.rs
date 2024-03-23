02e0cb9060aa605928ca08322247ea89147757bf
0xantman <antho74830@gmail.com>
2024-03-24 00:33:12 +0100

    Update reward struction for rank

--- programs/battleboosters/src/state/event.rs
+++ programs/battleboosters/src/state/event.rs
@@ -90,6 +90,11 @@ pub struct EventData {
     pub rank_rewards: Vec<RankReward>,
 }
 
+/*
+   TODO:
+       Consider adding predefined tiers for rewards for simplicity and user predicable
+       rewards for fighters, boosters and champions pass
+*/
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
 pub struct RankReward {
     pub start_rank: u64,            //  Defines the beginning rank of a reward tier.
@@ -98,6 +103,34 @@ pub struct RankReward {
     pub fighter_amount: i16,   // Quantities of fighter in-game assets awarded
     pub booster_amount: i16,   // Quantities of booster in-game assets awarded
     pub champions_pass_amount: i16, // Quantities of champion's pass in-game assets awarded
+}
+/*
+      TODO: User tier as reward structure
+*/
+// Define a struct to hold the rewards for each tier.
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
+pub struct TierRewards {
+    prize_amount: u64,          // Currency or token reward
+    fighter_amount: i16,        // Quantities of fighter in-game assets awarded
+    booster_amount: i16,        // Quantities of booster in-game assets awarded
+    champions_pass_amount: i16, // Quantities of champions pass in-game assets awarded
+}
+
+// Enum to represent the different tiers.
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
+pub enum Tier {
+    Tier1,
+    Tier2,
+    Tier3,
+    Tier4,
+    Tier5,
+}
+
+// Struct to link tiers with their corresponding rewards.
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
+pub struct TierReward {
+    tier: Tier,
+    rewards: TierRewards,
 }
 
 /*
